148acf82ad9e12bd7b08fcbbc8a0642311049792
Zakariye Ali Alim <sakariyecali15@gmail.com>
2023-06-29 05:17:21 +0300

    after book added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'd.dart';
-
+import 'inheritance_rules.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: LiveHeirsScreen(),
+      body: MyApp(),
     ),
   ));
 }
